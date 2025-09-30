0778b99a249c0d678be586813e54f831861b7958
Douglas Saturnino <douglasaturnino@gmail.com>
2025-09-30 15:56:39 -0300

    Adiciona valor padrão ao parâmetro chat_id no endpoint de upload de CSV

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def process_csv(request: SendFileRequest, background_tasks: BackgroundTasks):
 
 @app.post("/upload_csv_stream")
 def upload_csv_stream(
-    chat_id: str,
+    chat_id: str = Form(...),
     file: UploadFile = File(...),
     background_tasks: BackgroundTasks = None,
 ):
